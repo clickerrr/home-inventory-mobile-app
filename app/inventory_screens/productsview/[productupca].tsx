5090de222175aac3399f9a7696968752d3373064
bartekswiech <bartekswiech1103@gmail.com>
2025-06-29 10:02:52 -0700

    Added edit product functionality
    Products are now editable. You can edit the name and the container type
    of a product.

--- app/inventory_screens/productsview/[productupca].tsx
+++ app/inventory_screens/productsview/[productupca].tsx
@@ -1,40 +1,111 @@
 import GlobalStyles from '@/styles/GlobalStyles';
 import { Product } from '@/types/Product';
 import { useLocalSearchParams } from 'expo-router';
+import AntDesign from '@expo/vector-icons/AntDesign';
 import { useEffect, useState } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import logo from '@/assets/images/temp_product_image.jpg';
 import ProductPageStyles from '@/styles/ProductPageStyles';
+import axios from 'axios';
+import ProductEditForm from '@/components/ProductEditForm';
 
 const IndividualProductPage = () => {
     const { productupca, product } = useLocalSearchParams();
     const [productDetails, setProductDetails] = useState<Product | undefined>(
         undefined
     );
+    const [productContainer, setProductContainer] = useState<string>('');
+    const [productTitle, setProductTitle] = useState<string>('');
     const [isLoading, setIsLoading] = useState(true);
+    const [isEditing, setIsEditing] = useState<boolean>(false);
     useEffect(() => {
+        setIsLoading(true);
         console.log(productupca);
         console.log('CURRENT PRODUCT', product);
         if (product === undefined) return;
         const parsedProduct: Product = JSON.parse(product);
-        setProductDetails(parsedProduct);
-        setIsLoading(false);
         console.log('Product: ', product);
+        fetchFullProductDetails(parsedProduct.upca);
     }, [productupca, product]);
+
+    const fetchFullProductDetails = (upca: string) => {
+        const baseUrl = process.env.EXPO_PUBLIC_BASE_URL;
+
+        console.log('passed upca: ', upca);
+        axios
+            .get(`${baseUrl}/products/${upca}`)
+            .then((response) => {
+                const productData: Product = response.data;
+                console.log(productData);
+                setProductDetails(productData);
+                setProductTitle(productData.title);
+                setProductContainer(productData.containerType);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                if (error.status == 404) {
+                    console.error('Product UPCA is invalid');
+                }
+                setIsLoading(false);
+            });
+    };
+
     if (isLoading) {
-        return <></>;
+        return (
+            <View>
+                <Text>Loading...</Text>
+            </View>
+        );
     }
+
+    const remoteUpdateProduct = (updatedProduct) => {
+        const baseUrl = process.env.EXPO_PUBLIC_BASE_URL;
+        console.log('remote updating product', updatedProduct);
+        axios.put(`${baseUrl}/products/${updatedProduct.upca}`, updatedProduct);
+    };
+
     return (
         <View style={GlobalStyles.container}>
-            <Image style={ProductPageStyles.productImage} source={logo} />
             <View style={ProductPageStyles.content}>
-                <Text style={GlobalStyles.headerText}>
-                    {productDetails?.title}
-                </Text>
-                <Text style={GlobalStyles.subHeader}>UPCA</Text>
-                <Text style={GlobalStyles.text}>{productDetails?.upca}</Text>
-                <Text style={GlobalStyles.subHeader}>Container Type</Text>
-                <Text>{productDetails?.containerType}</Text>
+                <View style={ProductPageStyles.topContainer}>
+                    <TouchableOpacity onPress={() => setIsEditing(true)}>
+                        <AntDesign size={32} name="edit" color={'black'} />
+                    </TouchableOpacity>
+                </View>
+                <Image style={ProductPageStyles.productImage} source={logo} />
+                {isEditing ? (
+                    <>
+                        <ProductEditForm
+                            productTitle={productTitle}
+                            containerType={productContainer}
+                            onSubmit={(newTitle, newContainerType) => {
+                                setProductTitle(newTitle);
+                                setProductContainer(newContainerType);
+                                setIsEditing(false);
+                                productDetails.title = newTitle;
+                                productDetails.containerType = newContainerType;
+                                remoteUpdateProduct(productDetails);
+                            }}
+                            onCancel={() => {
+                                setIsEditing(false);
+                            }}
+                        />
+                    </>
+                ) : (
+                    <>
+                        <Text style={GlobalStyles.headerText}>
+                            {productTitle}
+                        </Text>
+                        <Text style={GlobalStyles.subHeader}>UPCA</Text>
+                        <Text style={GlobalStyles.text}>
+                            {productDetails?.upca}
+                        </Text>
+                        <Text style={GlobalStyles.subHeader}>
+                            Container Type
+                        </Text>
+                        <Text>{productContainer}</Text>
+                    </>
+                )}
             </View>
         </View>
     );
---
+++ components/ProductEditForm.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+
+import { Text, TouchableOpacity, View, TextInput } from 'react-native';
+import ProductAdderStyles from '@/styles/ProductAdderStyles';
+import ContainerTypeDropdown from '@/components/ContainerTypeDropdown';
+import GlobalStyles from '@/styles/GlobalStyles';
+import ProductEditFormStyles from '@/styles/ProductEditFormStyles';
+interface ProductEditFormProps {
+    productTitle: string;
+    containerType: string;
+    onSubmit: (newTitle: string, newContainerType: string) => void;
+    onCancel: () => void;
+}
+const ProductEditForm = ({
+    productTitle,
+    containerType,
+    onSubmit,
+    onCancel,
+}: ProductEditFormProps) => {
+    useEffect(() => {
+        setProductTitleText(productTitle);
+        setProductContainerType(containerType);
+    }, []);
+    const [productTitleText, setProductTitleText] = useState<string>('');
+    const [productTitleTextFocused, setProductTitleTextFocused] =
+        useState<boolean>(false);
+    const [productContainerType, setProductContainerType] =
+        useState<string>('');
+    return (
+        <View style={ProductEditFormStyles.container}>
+            <TextInput
+                style={[ProductAdderStyles.input, ProductEditFormStyles.item]}
+                placeholder="Enter product name..."
+                placeholderTextColor="#999"
+                value={productTitleText}
+                onChangeText={setProductTitleText}
+                onFocus={() => setProductTitleTextFocused(true)}
+                onBlur={() => setProductTitleTextFocused(false)}
+            />
+            <ContainerTypeDropdown
+                containerStyle={ProductEditFormStyles.item}
+                containerType={productContainerType}
+                setContainerType={setProductContainerType}
+            />
+            <TouchableOpacity
+                style={[GlobalStyles.buttonMain, ProductEditFormStyles.item]}
+                onPress={() => onSubmit(productTitleText, productContainerType)}
+            >
+                <Text style={GlobalStyles.buttonText}>Submit</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={[GlobalStyles.buttonMain, ProductEditFormStyles.item]}
+                onPress={onCancel}
+            >
+                <Text style={GlobalStyles.buttonText}>Cancel</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+export default ProductEditForm;
