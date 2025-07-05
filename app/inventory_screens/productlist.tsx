97af8bc37dbb0e91de3ff2ebfaf8ec4be74a4ba9
bartekswiech <bartekswiech1103@gmail.com>
2025-07-05 12:06:04 -0700

    Cleaned up imports

--- app/inventory_screens/productlist.tsx
+++ app/inventory_screens/productlist.tsx
@@ -1,13 +1,10 @@
 import GlobalStyles from '@/styles/GlobalStyles';
-import { useEffect, useState, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import { FlatList, Text, TouchableOpacity, View } from 'react-native';
-import productSampleData from '@/sampleData/ProductSampleData';
 import { Product } from '@/types/Product';
 import InventoryProductStyles from '@/styles/InventoryProductStyles';
 import { router, useFocusEffect } from 'expo-router';
 import axios from 'axios';
-import ProductListFolder from '@/components/ProductListFolder';
-import ProductListElement from '@/components/ProductListElement';
 import { sortObjectsById } from '@/utils/SortObjects';
 
 const ProductList = () => {
