2b4db709fd9157adc9d38c48929dbced8b4ddc5b
bartekswiech <bartekswiech1103@gmail.com>
2025-08-29 15:48:27 -0700

    Added logout functionality

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,6 +1,7 @@
 import GlobalStyles from '@/styles/GlobalStyles';
 import { toLog } from '@/utils/ConsoleLog';
-import { readFromKeyStore } from '@/utils/KeyStore';
+import { deleteFromKeystore, readFromKeyStore } from '@/utils/KeyStore';
+import { router } from 'expo-router';
 import { View, Text, TouchableOpacity } from 'react-native';
 
 const Settings = () => {
@@ -23,6 +24,16 @@ const Settings = () => {
                     }}
                 >
                     <Text style={GlobalStyles.buttonText}>Print Refresh Token</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={GlobalStyles.buttonMain}
+                    onPress={() => {
+                        deleteFromKeystore('himas_refreshToken');
+                        deleteFromKeystore('himas_authToken');
+                        router.replace('/login');
+                    }}
+                >
+                    <Text style={GlobalStyles.buttonText}>Logout</Text>
                 </TouchableOpacity>
             </View>
         );
--- utils/KeyStore.ts
+++ utils/KeyStore.ts
@@ -6,3 +6,8 @@ export const readFromKeyStore = (key: string): any => {
     const result = SecureStore.getItem(key);
     return result;
 };
+export const deleteFromKeystore = async (key: string): Promise<any> => {
+    await SecureStore.deleteItemAsync(key);
+    const deletionCheck = readFromKeyStore(key);
+    return deletionCheck === null;
+};
